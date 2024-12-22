50def0f65b46b1ccab2f8950a56450c19bd5aec4
Mizael <139066071+mizaladeasd@users.noreply.github.com>
2024-12-22 18:36:38 -0300

    Add files via upload

---
+++ ws-aulas/userprod/src/main/java/com/produtos/userprod/UserprodApplication.java
@@ -0,0 +1,12 @@
+package com.produtos.userprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserprodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserprodApplication.class, args);
+    }
+}
---
+++ ws-aulas/userprod/src/test/java/com/produtos/userprod/UserprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtos.userprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
